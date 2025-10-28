3bfcf0fb02ea87eaee6ae0bfe01d9edeedd34121
mohammad <fm1375md@gmail.com>
2025-10-28 22:30:11 +0330

    اولین کامیت درست

---
+++ age.py
@@ -0,0 +1,7 @@
+age=int(input("Enter your exact age based on your birth year. Example: 1388:"))
+h=age*365*24
+m=h*60
+s=h*60
+print("Your age in hours:", h)
+print("Your age in minutes:", m)
+print("Your age in seconds:", s)
